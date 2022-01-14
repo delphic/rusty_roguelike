05ca0f5eca6011d5366e3e2d50de1142c6a09034
delphic <delphic.uk@gmail.com>
2022-01-14 11:33:58 +0000

    Added self healing

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)] // Don't actually think we need this?
+#[write_component(Health)]
 pub fn player_input(
 	sub_world: &mut SubWorld,
 	#[resource] key: &Option<VirtualKeyCode>,
@@ -26,20 +28,25 @@ pub fn player_input(
 				.find_map(|(entity, pos)| Some((*entity, *pos + delta)))
 				.unwrap();
 
-			let mut is_attacking = false;
+			
 			let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
 			if delta.x != 0 || delta.y != 0 {
+				let mut is_attacking = false;
 				enemies
 					.iter(sub_world)
 					.filter(|(_, pos)| { **pos == destination })
 					.for_each(|(entity, _)| {
 						is_attacking = true;
 						commands.push(((), WantsToAttack { attacker: player_entity, victim: *entity }));
-					})
-			}
-
-			if !is_attacking {
-				commands.push(((), WantsToMove { entity: player_entity, destination }));
+					});
+				
+				if !is_attacking {
+					commands.push(((), WantsToMove { entity: player_entity, destination }));
+				}
+			} else {
+				if let Ok(mut health) = sub_world.entry_mut(player_entity).unwrap().get_component_mut::<Health>() {
+					health.current = i32::min(health.max, health.current + 1);
+				}
 			}
 
 			*turn_state = TurnState::PlayerTurn;
